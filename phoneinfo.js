c5051d984d9c15614d9f3e8512153301ebaa3e8f
lekhonkhan123 <lekhonkhan906@gmail.com>
2022-03-01 18:37:34 +0600

    first commit

---
+++ phoneinfo.js
@@ -0,0 +1,28 @@
+const moreinfo = () => {
+    const searchField = document.getElementById('seach-field')
+    const seachtext = searchField.value = "";
+    const url = `https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-11089 = {search text}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data.meals))
+
+
+    const moreinfo = () => {
+        const searchField = document.getElementById('seach-field')
+        const seachtext = searchField.value = "";
+        const url = `https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-11089 = {search text}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => console.log(data.meals))
+
+        const moreinfo = () => {
+            const searchField = document.getElementById('seach-field')
+            const seachtext = searchField.value = "";
+            const url = `https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-11089 = {search text}`;
+            fetch(url)
+                .then(res => res.json())
+                .then(data => console.log(data.meals))
+
+
+        }
+    }
